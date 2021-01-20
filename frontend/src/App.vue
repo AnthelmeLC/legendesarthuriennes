92941d8bf63a01f4e8c62a438e52a96872ec766d
AnthelmeLC <toine17@gmail.com>
2021-01-20 19:17:23 +0100

    ajout de la connexion déconnexion utilisateur + réindentation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,27 +5,27 @@
         <router-link to="/preface">Préface</router-link>
         <a class="expand">
             Personnages
-            <div class="hidden">
+            <div class="hidden unfold">
                 <a>Merlin l'enchanteur</a>
             </div>
         </a>
         <a class="expand">
             Lieux
-            <div class="hidden">
+            <div class="hidden unfold">
                 <a>test</a>
                 <a>test encore</a>
             </div>
         </a>
         <a class="expand">
             Objets
-            <div class="hidden">
+            <div class="hidden unfold">
                 <a>test</a>
                 <a>test encore</a>
             </div>
         </a>
         <a class="expand">
             Évènements
-            <div class="hidden">
+            <div class="hidden unfold">
                 <a>test</a>
                 <a>test encore</a>
             </div>
@@ -33,146 +33,200 @@
         <router-link to="/bibliography">Bibliograhie</router-link>
         <router-link to="/about">À propos</router-link>
         <router-link to="/contact">Contact</router-link>
+        <router-link to="/login" id="login">Connexion</router-link>
+        <router-link to="/user" id="userSpaceLink">Espace Auteur</router-link>
+        <a id="disconnect">Déconnexion</a>
       </div>
       <router-view/>
     </div>
 </template>
 
 <style>
-/*STYLES ET CLASSES GENERAUX*/
-@font-face{
-    font-family: "Mirella Script";
-    src:url(./assets/mirellascript.ttf) format("truetype");
-}
+    /*STYLES ET CLASSES GENERAUX*/
+    @font-face{
+        font-family: "Mirella Script";
+        src:url(./assets/mirellascript.ttf) format("truetype");
+    }
 
-@font-face{
-    font-family: "KingthingsCalligraphicaLight";
-    src:url(./assets/kingthingscalligraphicalight.ttf) format("truetype");
-}
+    @font-face{
+        font-family: "KingthingsCalligraphicaLight";
+        src:url(./assets/kingthingscalligraphicalight.ttf) format("truetype");
+    }
 
-@font-face{
-    font-family: "GothicUltra";
-    src:url(./assets/gothicultra.ttf) format("truetype");
-}
+    @font-face{
+        font-family: "GothicUltra";
+        src:url(./assets/gothicultra.ttf) format("truetype");
+    }
 
-*{
-    box-sizing: border-box;
-}
+    *{
+        box-sizing: border-box;
+    }
 
-html, body{
-    margin: 0;
-    padding: 0;
-    background-color: #cc9a46;
-}
+    html, body{
+        margin: 0;
+        padding: 0;
+        background-color: #cc9a46;
+    }
 
-h1{
-    font-family: Mirella Script;
-    font-size: 3em;
-    text-decoration: underline;
-    text-align: center;
-}
+    h1{
+        font-family: Mirella Script;
+        font-size: 4em;
+        font-weight: lighter;
+        text-decoration: underline;
+        text-align: center;
+    }
 
-p{
-    font-size: x-large;
-    width: 50%;
-    margin-left: auto;
-    margin-right: auto;
-    margin-bottom: 5%;
-}
+    p{
+        font-size: 2em;
+        width: 75%;
+        margin-left: auto;
+        margin-right: auto;
+        margin-bottom: 5%;
+    }
 
-.container{
-    max-width: 1000px;
-    min-height: 100vh;
-    margin-right: auto;
-    margin-left: auto;
-    padding-top: 1px;
-    padding-bottom: 20px;
-    background-color: #efefef;
-}
+    .container{
+        max-width: 1000px;
+        min-height: 100vh;
+        margin-right: auto;
+        margin-left: auto;
+        padding-top: 1px;
+        padding-bottom: 20px;
+        background-color: #efefef;
+    }
 
-.hidden{
-    display: none;
-}
+    .hidden{
+        display: none;
+    }
 
-/*STYLES APP ET NAV*/
-#app{
-    font-family: KingthingsCalligraphicaLight, Times New Roman, Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: black;
-    position: relative;
-}
+    #app{
+        font-family: KingthingsCalligraphicaLight, Times New Roman, Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        color: black;
+        position: relative;
+    }
 
-#nav{
-    font-family: GothicUltra;
-    width: 10%;
-    position: fixed;
-    top: 0;
-    left: 0;
-    font-size: 2em;
-    background-color: #efefef;
-}
+    button{
+            width: 100px;
+            height: 50px;
+            border-radius: 25%;
+            border: none;
+            font-family: KingthingsCalligraphicaLight;
+            font-size: 1.5em;
+            position: absolute;
+            right: 0;
+            background-color: #2d6ca2;
+            color: white;
+            cursor: pointer;
+    }
 
-#nav a{
-    display: block;
-    color: black;
-    text-decoration: none;
-    margin: 0% auto 5% auto;
-    padding-top: 5%;
-    position: relative;
-    padding-left: 10px;
-}
+    button:hover{
+        background-color: #1F496E;
+        transition: all 400ms ease-in-out;
+    }
+</style>
 
-#nav a::after{
-    content: "";
-    position: absolute;
-    left: 0;
-    bottom: 0;
-    transform: scaleX(0);
-    transform-origin: left;
-    width: 95%;
-    height: 2px;
-    background-color: red;
-    transition: transform 400ms ease-out;
-}
+<style scoped>
+    /*STYLES NAV*/
+    #nav{
+        font-family: GothicUltra;
+        width: 10%;
+        position: fixed;
+        top: 0;
+        left: 0;
+        font-size: 2em;
+        background-color: #efefef;
+    }
 
-#nav a:hover::after{
-    transform: scaleX(1);
-}
+    a{
+        display: block;
+        color: black;
+        text-decoration: none;
+        margin: 0% auto 5% auto;
+        padding-top: 5%;
+        position: relative;
+        padding-left: 10px;
+    }
 
-#nav a.router-link-exact-active{
-    color: #1000e6;
-}
+    a::after{
+        content: "";
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        transform: scaleX(0);
+        transform-origin: left;
+        width: 95%;
+        height: 2px;
+        background-color: red;
+        transition: transform 400ms ease-out;
+    }
 
-.expand{
-    position: relative;
-}
+    a:hover::after{
+        transform: scaleX(1);
+    }
 
-.expand:hover{
-    cursor: pointer;
-}
+    a.router-link-exact-active{
+        color: #1000e6;
+    }
 
-.expand:hover div{
-    display: block;
-    cursor: pointer;
-}
+    .expand{
+        position: relative;
+    }
 
-#nav .hidden{
-    position: absolute;
-    left: 100%;
-    top: 0%;
-    background-color: #efefef;
-    width: 100%;
-}
+    .expand:hover{
+        cursor: pointer;
+    }
 
-#nav .hidden div{
-    margin: 0% auto 5% auto;
-    padding-top: 5%;
-}
+    .expand:hover div{
+        display: block;
+        cursor: pointer;
+    }
+
+    .unfold{
+        position: absolute;
+        left: 100%;
+        top: 0%;
+        background-color: #efefef;
+        width: 100%;
+    }
+
+    .unfold div{
+        margin: 0% auto 5% auto;
+        padding-top: 5%;
+    }
+
+    #disconnect{
+        cursor: pointer;
+    }
 </style>
 
 <script>
-export default {
-    name: 'App'
-}
+    export default {
+        name: 'App',
+        mounted(){
+            const disconnect = document.getElementById("disconnect");
+            //si utilisateur n'est pas connecté
+            if(!localStorage.token){
+                //on cache le lien vers l'espace utilisateur et le lien de déconnexion
+                const userSpaceLink = document.getElementById("userSpaceLink");
+                userSpaceLink.setAttribute("style", "display : none");
+                disconnect.setAttribute("style", "display : none");
+            }
+            else{
+                //on cache le boutton de connexion
+                const login = document.getElementById("login");
+                login.setAttribute("style", "display : none");
+                
+            }
+    
+            //boutton déconnexion
+            disconnect.addEventListener("click", function(e){
+                e.preventDefault();
+                //suppression du token d'authentification, tu userId et admin
+                localStorage.clear();
+                window.location.reload();
+                return false
+            })
+        }
+    }
 </script>
